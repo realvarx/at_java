e73a7e71aa691ad9671bec564636bf979b698875
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-07-15 21:06:39 -0400

    feat: Public/Self keys Get, Put, and Delete examples

---
+++ at_client/src/test/java/org/atsign/common/examples/SelfKeyGetExample.java
@@ -0,0 +1,47 @@
+package org.atsign.common.examples;
+
+import java.util.concurrent.ExecutionException;
+
+import org.atsign.client.api.AtClient;
+import org.atsign.common.AtException;
+import org.atsign.common.AtSign;
+import org.atsign.common.KeyBuilders;
+import org.atsign.common.Keys.SelfKey;
+
+public class SelfKeyGetExample {
+    
+    public static void main(String[] args) {
+        // 1. establish constants
+        String ROOT_URL = "root.atsign.wtf:64";
+        String ATSIGN_STR = "@33thesad";
+        boolean VERBOSE = true;
+        
+        String KEY_NAME = "test";
+        
+        // 2. create AtSign object
+        AtSign atSign = new AtSign(ATSIGN_STR);
+        
+        // 3. atClient factory method
+        AtClient atClient = null;
+        try {
+            atClient = AtClient.withRemoteSecondary(ROOT_URL, atSign, VERBOSE);
+        } catch (AtException e) {
+            System.err.println("Failed to connect to remote server " + e);
+            e.printStackTrace();
+        }
+        
+        // 4. create selfkey
+        SelfKey sk = new KeyBuilders.SelfKeyBuilder(atSign).key(KEY_NAME).build();
+        
+        // 5. get the key
+        String response = null;
+        try {
+            response = atClient.get(sk).get();
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println("Failed to get key " + e);
+            e.printStackTrace();
+        }
+        System.out.println(response);
+    }
+
+}
